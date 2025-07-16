00a2afbeb8f4d7707e0b24764e2efb9886de5e7b
RIKO910 <tarikulislamriko910@gmail.com>
2025-07-17 00:13:49 +0600

    name change

--- flag-view.php
+++ flag-view.php
@@ -1,66 +1,102 @@
 <?php
-/*
-Plugin Name: Country Flags Trending
-Plugin URI: https://yourwebsite.com
-Description: Display country flags with jumping animation on hover
-Version: 1.0
-Author: Your Name
-Author URI: https://yourwebsite.com
-License: GPL2
-*/
+/**
+ * Plugin Name: Flag View
+ * Plugin URI: https://wordpress.org/plugins/flag-view/
+ * Description: Display interactive country flags with names and fun hover animations. Perfect for multilingual sites, travel blogs, and country-specific content.
+ * Version: 1.0.0
+ * Requires at least: 6.5
+ * Requires PHP: 7.2
+ * Author: Tarikul Islam Riko
+ * Author URI: https://tarikul.blog
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: flag-view
+ */
 
 // Exit if accessed directly
 if (!defined('ABSPATH')) {
     exit;
 }
 
+// Define plugin constants
+define('FLAG_VIEW_VERSION', '1.0.0');
+define('FLAG_VIEW_PATH', plugin_dir_path(__FILE__));
+define('FLAG_VIEW_URL', plugin_dir_url(__FILE__));
+
 // Enqueue scripts and styles
-function cft_enqueue_scripts() {
-    wp_enqueue_style('cft-style', plugins_url('assets/css/style.css', __FILE__));
-    wp_enqueue_script('cft-script', plugins_url('assets/js/script.js', __FILE__), array('jquery'), '1.0', true);
+function flag_view_enqueue_assets() {
+    wp_enqueue_style(
+        'flag-view-style',
+        FLAG_VIEW_URL . 'assets/css/style.css',
+        array(),
+        FLAG_VIEW_VERSION
+    );
+
+    wp_enqueue_script(
+        'flag-view-script',
+        FLAG_VIEW_URL . 'assets/js/script.js',
+        array('jquery'),
+        FLAG_VIEW_VERSION,
+        true
+    );
 }
-add_action('wp_enqueue_scripts', 'cft_enqueue_scripts');
+add_action('wp_enqueue_scripts', 'flag_view_enqueue_assets');
 
 // Shortcode to display flags
-function cft_display_flags($atts) {
+function flag_view_shortcode($atts) {
     $atts = shortcode_atts(array(
         'countries' => 'us,gb,ca,au,de,fr,jp,br,in',
-        'size' => 'medium'
-    ), $atts);
+        'size' => 'medium',
+        'animation' => 'jump'
+    ), $atts, 'flag_view');
 
     $countries = explode(',', $atts['countries']);
-    $output = '<div class="cft-flags-container">';
+    $output = '<div class="flag-view-container">';
 
     foreach ($countries as $country) {
         $country = trim(strtolower($country));
-        $flag_url = plugins_url("flags/{$country}.png", __FILE__);
-        $country_name = cft_get_country_name($country);
+        $flag_url = FLAG_VIEW_URL . "flags/{$country}.png";
+        $country_name = flag_view_get_country_name($country);
 
-        $output .= '<div class="cft-flag-item" data-country="' . esc_attr($country) . '">';
-        $output .= '<img src="' . esc_url($flag_url) . '" alt="' . esc_attr($country_name) . '" class="cft-flag cft-size-' . esc_attr($atts['size']) . '">';
-        $output .= '<span class="cft-country-name">' . esc_html($country_name) . '</span>';
-        $output .= '</div>';
+        $output .= sprintf(
+            '<div class="flag-view-item" data-country="%s" data-animation="%s">
+                <img src="%s" alt="%s" class="flag-view-flag flag-view-size-%s">
+                <span class="flag-view-country-name">%s</span>
+            </div>',
+            esc_attr($country),
+            esc_attr($atts['animation']),
+            esc_url($flag_url),
+            esc_attr($country_name),
+            esc_attr($atts['size']),
+            esc_html($country_name)
+        );
     }
 
     $output .= '</div>';
     return $output;
 }
-add_shortcode('country_flags', 'cft_display_flags');
+add_shortcode('flag_view', 'flag_view_shortcode');
 
 // Helper function to get country name from code
-function cft_get_country_name($code) {
+function flag_view_get_country_name($code) {
     $countries = array(
-        'us' => 'United States',
-        'gb' => 'United Kingdom',
-        'ca' => 'Canada',
-        'au' => 'Australia',
-        'de' => 'Germany',
-        'fr' => 'France',
-        'jp' => 'Japan',
-        'br' => 'Brazil',
-        'in' => 'India',
+        'us' => __('United States', 'flag-view'),
+        'gb' => __('United Kingdom', 'flag-view'),
+        'ca' => __('Canada', 'flag-view'),
+        'au' => __('Australia', 'flag-view'),
+        'de' => __('Germany', 'flag-view'),
+        'fr' => __('France', 'flag-view'),
+        'jp' => __('Japan', 'flag-view'),
+        'br' => __('Brazil', 'flag-view'),
+        'in' => __('India', 'flag-view'),
         // Add more countries as needed
     );
 
     return isset($countries[$code]) ? $countries[$code] : ucfirst($code);
 }
+
+// Load textdomain
+function flag_view_load_textdomain() {
+    load_plugin_textdomain('flag-view', false, dirname(plugin_basename(__FILE__)) . '/languages/');
+}
+add_action('plugins_loaded', 'flag_view_load_textdomain');
---
+++ readme.txt
@@ -0,0 +1,62 @@
+=== Flag View ===
+Contributors: riko910
+Tags: flags, country, animation, hover, interactive
+Requires at least: 6.5
+Tested up to: 6.5
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+Display interactive country flags with names and fun hover animations. Perfect for multilingual sites, travel blogs, and country-specific content.
+
+== Description ==
+
+Flag View is a lightweight WordPress plugin that lets you display beautiful country flags with interactive hover animations.
+
+Key features:
+- Display multiple country flags with names
+- Three size options (small, medium, large)
+- Fun hover animations (jump, shake, fade)
+- Easy to use shortcode implementation
+- Fully responsive design
+- Lightweight and fast
+
+Perfect for:
+- Multilingual websites
+- Travel blogs
+- Country-specific content
+- Educational websites
+- Event pages with international attendees
+
+== Installation ==
+
+1. Upload the `flag-view` folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Use the [flag_view] shortcode in your posts, pages, or widgets
+
+== Frequently Asked Questions ==
+
+= How do I use the plugin? =
+Simply use the [flag_view] shortcode in any post, page, or widget. You can customize it with parameters like:
+[flag_view countries="us,gb,fr" size="large" animation="jump"]
+
+= Where can I get support? =
+Please visit the [support forums](https://wordpress.org/support/plugin/flag-view/).
+
+= Can I add more countries? =
+Yes! Just add the appropriate flag PNG file to the flags directory and extend the country list in the code.
+
+== Screenshots ==
+1. Flag View plugin showing multiple country flags
+2. Hover effect with country name display
+3. Different size options demonstration
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial release
+
+== Upgrade Notice ==
+
+= 1.0.0 =
+Initial release of Flag View plugin.
